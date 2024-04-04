e1b407d5bca2191edfc6b6d213da644fe05f3aeb
liujingxing <18268800083ljx@gmail.com>
2024-04-04 23:41:37 +0800

    方法提取，并增加测试用例

---
+++ library/src/main/java/androidx/recyclerview/widget/Utils.kt
@@ -0,0 +1,35 @@
+package androidx.recyclerview.widget
+
+/**
+ * User: ljx
+ * Date: 2024/4/4
+ * Time: 23:10
+ */
+fun IntArray.rangeRemoved(positionStart: Int, count: Int): Boolean {
+    //remove [positionStart, positionEnd) range
+    val positionEnd: Int = positionStart + count
+    var changed = false
+    forEachIndexed { index, value ->
+        if (value >= positionStart) {
+            val offset = if (value >= positionEnd) {
+                count
+            } else {
+                value - positionStart + 1
+            }
+            set(index, get(index) - offset)
+            changed = true
+        }
+    }
+    return changed
+}
+
+fun IntArray.rangeInsert(positionStart: Int, itemCount: Int): Boolean {
+    var changed = false
+    forEachIndexed { index, value ->
+        if (positionStart <= value || value == -1 || index == lastIndex && value + 1 == positionStart) {
+            set(index, get(index) + itemCount)
+            changed = true
+        }
+    }
+    return changed
+}
--- library/src/test/java/com/ljx/layoutmanager/ExampleUnitTest.kt
+++ library/src/test/java/com/ljx/layoutmanager/ExampleUnitTest.kt
@@ -1,8 +1,9 @@
 package com.ljx.layoutmanager
 
+import androidx.recyclerview.widget.rangeInsert
+import androidx.recyclerview.widget.rangeRemoved
+import org.junit.Assert.assertArrayEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +12,32 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun testRangeRemoved() {
+        val array = intArrayOf(19, 22, 42, 50)
+        array.rangeRemoved(40, 10)
+        assertArrayEquals(intArrayOf(19, 22, 39, 40), array)
+
+        val array1 = intArrayOf(19, 22, 42, 50)
+        array1.rangeRemoved(43, 8)
+        assertArrayEquals(intArrayOf(19, 22, 42, 42), array1)
+
+        val array2 = intArrayOf(19, 22, 42, 50)
+        array2.rangeRemoved(50, 1)
+        assertArrayEquals(intArrayOf(19, 22, 42, 49), array2)
+    }
+
+    @Test
+    fun testRangeInsert() {
+        val array = intArrayOf(19, 22, 42, 50)
+        array.rangeInsert(51, 10)
+        assertArrayEquals(intArrayOf(19, 22, 42, 60), array)
+
+        val array1 = intArrayOf(19, 22, 42, 50)
+        array1.rangeInsert(40, 8)
+        assertArrayEquals(intArrayOf(19, 22, 50, 58), array1)
+
+        val array2 = intArrayOf(19, 22, 42, 50)
+        array2.rangeInsert(0, 25)
+        assertArrayEquals(intArrayOf(44, 47, 67, 75), array2)
     }
 }
